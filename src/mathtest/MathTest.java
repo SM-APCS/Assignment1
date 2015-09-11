fe166f8996378e5c067898cf3f56ebb75277be52
l1onh3art88 <spenccheng@gmail.com>
2015-09-11 08:54:34 -0500

    First Commit

---
+++ src/mathtest/MathTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathtest;
+
+/**
+ *
+ * @author spenc_000
+ */
+public class MathTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;
+        int y = 200;
+        System.out.println("The sum of 100 + 200 = " + (x + y));
+        System.out.println("Eight multiplied by seven = " + (8*7));
+        System.out.println("The quotient of 200 divided by 100 = " + (200/100));
+        System.out.println("The remainder of 10 /    4 is = " + (10%4));
+
+    }
+    
+}
