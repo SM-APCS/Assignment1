80aed98c91afe7f9ca56ccd704e31ed3d72c82af
Alex <Alex@10.1.30.130>
2015-09-11 11:30:27 -0500

    First Project-Alex

---
+++ src/variabletest/VariableTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variabletest;
+
+/**
+ *
+ * @author Alex
+ */
+public class VariableTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int one = 0;
+        int two = 0;
+        System.out.println ("one is set to " + one + "\n" + "two is set to " + two);
+        one = 1;
+        two = 2;
+        System.out.println ("one is now " + one + "\n" + "two is now " + two);
+  
+        
+                
+        // TODO code application logic here
+    }
+    
+}
